d7520f253ab8200b31b60fb58645dd1101e2761a
alperenturksoy <alperenturksoy@ALPERENTURKSOY>
2023-10-10 16:23:18 +0300

    Add project files.

---
+++ data.js
@@ -0,0 +1,22 @@
+module.exports = function () {
+  return {
+    products:[
+      { id : 1, name: "Iphone 8", price: 15000, imageurl: "ip8.jpg", description: "Yeni" },
+      { id : 2, name: "Iphone 8 plus", price: 20000, imageurl: "ip8_2.jpg", description: "Yeni" },
+      { id : 3, name: "Iphone 8 pro", price: 22000, imageurl: "ip8_3.jpg", description: "Yeni" },
+      { id : 4, name: "Iphone x", price: 25000, imageurl: "ipx.jpg", description: "Yeni" },
+      { id : 5, name: "Iphone x pro", price: 27000, imageurl: "ipx_2.jpg", description: "Yeni"},
+      { id : 6, name: "Iphone 11 pro", price: 29000, imageurl: "ip11.jpg", description: "Yeni" },
+      { id : 7, name: "Iphone 12pro", price: 33000, imageurl: "ip12.jpg", description: "Yeni" },
+      { id : 8, name: "Iphone 13 pro", price: 35000, imageurl: "ip13.jpg", description: "Yeni" }
+
+    ],
+    categories:[
+      { id: 1, name: "Telefon" },
+      { id: 2, name: "Bilgisayar" },
+      { id: 3, name: "Tablet" }
+      ],
+    orders:[]
+}
+ 
+}
